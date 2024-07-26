16e4e599ba441a6e4fafe206c2b70caa8ea23e71
Eric Mengelkoch <ericmengelkoch@gmail.com>
2024-07-26 15:47:28 -0500

    Completed calculator

--- script.js
+++ script.js
@@ -1,59 +1,130 @@
 function add(a, b) {
     return a + b;
-}
+};
 
 function subtract(a, b) {
     return a - b;
-}
+};
 
-function multiply(a , b) {
+function multiply(a, b) {
     return a * b;
-}
+};
 
 function divide(a, b) {
     return a / b;
-}
+};
 
 let oldNum = "reset";
 let newNum = "reset";
 let operator = "";
+let typeInProg = false;
 
 let displayVal = "";
 
 function operate(op, first, last) {
+    let value = "";
     if (op == "plus") {
-        return add(first, last);
+        value = add(first, last);
     } else if (op == "minus") {
-        return subtract(first, last);
+        value = subtract(first, last);
     } else if (op == "multiply") {
-        return multiply(first, last);
+        value = multiply(first, last);
     } else if (op == "divide") {
-        return divide(first, last);
+        value = divide(first, last);
+        if (value == "Infinity") {
+            clearDisplay();
+            value = "You know you're not allowed to do that!";
+        }
     }
-}
+    console.log(value);
+    clearDisplay();
+    updateDisplay(value.toString());
+    typeInProg = false;
+    oldNum = "reset";
+    newNum = "reset";
+};
+
+function clearDisplay() {
+    const screen = document.querySelector(".screen");
+    displayVal = " ";
+    screen.textContent = displayVal;
+};
 
 function updateDisplay(input) {
     const screen = document.querySelector(".screen");
     displayVal = displayVal + input;
     screen.textContent = displayVal;
-}
+};
 
 const buttonNum = document.querySelectorAll(".number");
 buttonNum.forEach((button) => {
     button.addEventListener("click", () => {
         console.log(Number(button.id));
-        updateDisplay(button.id);
+        if (typeInProg) {
+            updateDisplay(button.id);
+        } else {
+            clearDisplay();
+            updateDisplay(button.id);
+        }
+        typeInProg = true;
     });
 });
 
 const buttonOperator = document.querySelectorAll(".operator");
 buttonOperator.forEach((button) => {
     button.addEventListener("click", () => {
+        oldNum = newNum;
         newNum = (displayVal);
         if (oldNum != "reset") {
-
+            operate(operator, Number(oldNum), Number(newNum));
+            operator = (button.id);
         } else {
             operator = (button.id);
         }
-    })
-})
+        typeInProg = false;
+    });
+});
+
+const buttonEquals = document.querySelector("#equals");
+buttonEquals.addEventListener("click", () => {
+    oldNum = newNum;
+    newNum = (displayVal);
+    if (oldNum != "reset") {
+        operate(operator, Number(oldNum), Number(newNum));
+    }
+    typeInProg = false;
+});
+
+const buttonClear = document.querySelector(".clear");
+buttonClear.addEventListener("click", () => {
+    clearDisplay();
+    updateDisplay("0");
+    oldNum = "reset";
+    newNum = "reset";
+    operator = "";
+    typeInProg = false;
+});
+
+const buttonToggle = document.querySelector(".toggle");
+buttonToggle.addEventListener("click", () => {
+    displayVal = Number(displayVal) * -1;
+    updateDisplay("");
+});
+
+const buttonDelete = document.querySelector(".delete");
+buttonDelete.addEventListener("click", () => {
+    displayVal = displayVal.slice(0, displayVal.length - 1);
+    updateDisplay("");
+});
+
+const buttonDecimal = document.querySelector(".decimal");
+buttonDecimal.addEventListener("click", () => {
+    if (!displayVal.includes("."))
+        if (typeInProg) {
+            updateDisplay(".");
+        } else {
+            clearDisplay();
+            updateDisplay(".");
+        }
+        typeInProg = true;
+});
